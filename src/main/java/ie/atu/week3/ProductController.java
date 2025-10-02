8d73dfd0cf58dcd12b16d0e22ff5ae119fffb7b4
Mia Acton <G00428601@atu.ie>
2025-10-02 14:32:31 +0100

    Added ProductController class with /hello endpoint and Product model class

---
+++ src/main/java/ie/atu/week3/ProductController.java
@@ -0,0 +1,15 @@
+package ie.atu.week3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/product")
+@RestController
+public class ProductController {
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
